2a71f42864148df58f8ec9941fedd5ae152dd764
marcusstenbeck <marcus.stenbeck@gmail.com>
2014-12-20 07:00:26 +0100

    Remove OpenGL-specific commands. Describe steps with comments.

--- app/js/app.js
+++ app/js/app.js
@@ -5,23 +5,15 @@ require([
 ) {
 
 	function draw() {
-		// SpritePtr sp;
-		// glClearColor(0, 0, 0.2, 1);
-		// glClear(GL_COLOR_BUFFER_BIT+GL_DEPTH_BUFFER_BIT);
-		// glEnable(GL_TEXTURE_2D);
-		// glEnable(GL_BLEND);
-		// glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 		// DrawBackground();
+		
 		// SpriteBehavior(); // Din kod!
-		// // Loop though all sprites. (Several loops in real engine.)
-		// sp = gSpriteRoot;
-		// do
-		// {
-		// HandleSprite(sp); // Callback in a real engine
-		// DrawSprite(sp);
-		// sp = sp->next;
-		// } while (sp != NULL);
-		// glutSwapBuffers();
+		
+		/** 
+		 *  Loop though all sprites. (Several loops in real engine.)
+		 *    HandleSprite(sp);
+		 *    DrawSprite(sp);
+		 */
 	}
 
 	function update() {
@@ -29,12 +21,19 @@ require([
 	}
 
 	function init() {
+		/**
+		 *  Load texture data
+		 */
 		// TextureData *sheepFace, *blackFace, *dogFace, *foodFace;
 		// LoadTGATextureSimple("bilder/leaves.tga", &backgroundTexID); // Bakgrund
 		// sheepFace = GetFace("bilder/sheep.tga"); // Ett fŒr
 		// blackFace = GetFace("bilder/blackie.tga"); // Ett svart fŒr
 		// dogFace = GetFace("bilder/dog.tga"); // En hund
 		// foodFace = GetFace("bilder/mat.tga"); // Mat
+
+		/**
+		 *  Create sprites
+		 */
 		// NewSprite(sheepFace, 100, 200, 1, 1);
 		// NewSprite(sheepFace, 200, 100, 1.5, -1);
 		// NewSprite(sheepFace, 250, 200, -1, 1.5);

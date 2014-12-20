4b91156ef504b4d4419de03dbaff8893fe222422
marcusstenbeck <marcus.stenbeck@gmail.com>
2014-12-20 06:55:36 +0100

    Create basic loop app structure

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,25 @@
-require([], function () {
-	console.log('base');
+require([
+	'lib/SpriteRec'
+], function (
+	SpriteRec
+) {
+
+	function draw() {
+		
+	}
+
+	function update() {
+		
+	}
+
+	function init() {
+		
+	}
+
+	init();
+	(function loop(time) {
+		update(time);
+		draw(time);
+		window.requestAnimationFrame(loop);
+	})();
 })
